05fcf69a85b8e438f983ae6573e82e4c0c8950c7
Alan Benavides <bruno@coastt.org>
2021-07-18 20:00:55 -0400

    change: rename main routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import { Application } from "express";
 
-import { router as cappuchino } from "../modules/cappuchino/routes";
+import { router as departments } from "../modules/departments/routes";
+import { router as notifications } from "../modules/notifications/routes";
 
 export default function (app: Application) {
-	app.use(cappuchino);
+	app.use(departments);
+	app.use(notifications);
 }
