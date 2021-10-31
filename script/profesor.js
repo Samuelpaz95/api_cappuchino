90ee54a103a51e9f3ec23174ebe63166f6b87923
Alan Benavides <bruno@coastt.org>
2021-10-30 21:27:33 -0400

    refactor script get profesors

--- script/profesor.js
+++ script/profesor.js
@@ -1,33 +1,27 @@
-const fs = require('fs');
+const fs = require("fs");
+const ROOT_FILE = "./public/data/departments/";
 
-fs.readFile ('./public/data/departments/index.json', (err, data) => { 
-    const faculty=[];   
-    if (err) throw err;
-    const faculties = JSON.parse(data);
-    for (const {semanticName: semanticName} of faculties) {
-        faculty.push({semanticName} );   
-    }
-    console.log(faculty)
-for (const {semanticName} of faculty){
-fs.readFile (`./public/data/departments/${semanticName}/index.json`, (error, data) => {
-    const materia=[];
-    if (error) throw error;
-    const materias = JSON.parse(data);
-    for (const {code: code} of materias) {
-        materia.push({code}); 
-    }
-    console.log(materia)
+const facultiesDataUnformated = fs.readFileSync(ROOT_FILE + "index.json");
+const facultiesData = JSON.parse(facultiesDataUnformated);
+const faculties = facultiesData.map(({ semanticName }) => semanticName);
 
-    for(const {code } of materia){
-        fs.readFile (`./public/data/departments/${semanticName}/2021-1/${code}.json`, (err, data) => {
-            const profesor=[];
-            if (err) throw err;
-            const profesors = JSON.parse(data);
-            //  for (const {name:name} of profesors) {
-            //       profesor.push({name }); 
-            //  }
-            // no logro ingresar a la informacion de los profesores 
-            console.log(profesors)
-        })}
-})};
-});
+const subjectInfo = faculties
+	.map((faculty) => fs.readFileSync(ROOT_FILE + `${faculty}/index.json`))
+	.map(JSON.parse)
+	.map((subject, i) =>
+		subject.map(({ code, semester }) => ({
+			code,
+			semester,
+			faculty: faculties[i],
+		}))
+	)
+	.flat();
+
+const levels = subjectInfo
+	.map(({ code, faculty, semester }) =>
+		fs.readFileSync(ROOT_FILE + `${faculty}/${semester}/${code}.json`)
+	)
+	.map(JSON.parse);
+// .map(({ levels }) => levels);
+
+console.log(levels);
