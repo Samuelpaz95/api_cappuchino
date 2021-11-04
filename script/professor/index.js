92631513388003c07d68ced6605e1e33651e410d
Alan Benavides <bruno@coastt.org>
2021-11-04 00:03:04 -0400

    add: get all teacjers

--- script/professor/index.js
+++ script/professor/index.js
@@ -1,4 +1,19 @@
 const { cleanSubjects } = require("./cleanSubjects");
+const { writeFileSync } = require("fs");
+const ROUTE = "./public/data/";
 
-const subjects = cleanSubjects();
-console.log(subjects);
+const subjectsByDepartments = cleanSubjects(ROUTE + "departments/");
+
+subjectsByDepartments.forEach((subjectsByDepartment) => {
+	const professors = new Set();
+	subjectsByDepartment.subjects.forEach((subject) => {
+		subject.groups.forEach(({ teacher }) => {
+			professors.add(teacher);
+		});
+	});
+
+	writeFileSync(
+		ROUTE + `professors/${subjectsByDepartment.deparment}/teachers.json`,
+		JSON.stringify([...professors])
+	);
+});
