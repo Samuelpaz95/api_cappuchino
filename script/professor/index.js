0994fe12554757d9d3efabf29287267c47118623
Alan Benavides <bruno@coastt.org>
2021-11-05 11:44:18 -0400

    add: files with professors with own subjects

--- script/professor/index.js
+++ script/professor/index.js
@@ -1,5 +1,5 @@
 const { cleanSubjects } = require("./cleanSubjects");
-const { professorsByDeparment } = require("./scraper");
+const { professorsByDeparment, subjectsByProfessors } = require("./scraper");
 const { writeFileSync } = require("fs");
 const ROUTE = "./public/data/";
 
@@ -10,5 +10,13 @@ Object.entries(professors).forEach(([deparment, professors]) => {
 	writeFileSync(
 		ROUTE + `professors/${deparment}/teachers.json`,
 		JSON.stringify(professors)
+	);
+});
+
+Object.entries(subjectsByDepartments).forEach(([deparment, subjects]) => {
+	const professorSubjects = subjectsByProfessors(subjects, professors[deparment]);
+	writeFileSync(
+		ROUTE + `professors/${deparment}/teachersContent.json`,
+		JSON.stringify(professorSubjects)
 	);
 });
