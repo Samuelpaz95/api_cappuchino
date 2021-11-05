8215cd24dd0c0d8da0d866ef9d26dd5bf88e8c70
Alan Benavides <bruno@coastt.org>
2021-11-05 14:15:45 -0400

    refactor: export interfaces directly

--- src/modules/departments/interfaces/index.ts
+++ src/modules/departments/interfaces/index.ts
@@ -1,6 +1,4 @@
-import { Icarrer, Idepartment, IdepartementCarrer } from "./app.interface";
-import { Imodel } from "./model.interface";
-import { Irepository } from "./repository.interface";
-import { Icontroller } from "./controller.interface";
-
-export { Icarrer, Idepartment, IdepartementCarrer, Irepository, Imodel, Icontroller };
+export { Icarrer, Idepartment, IdepartementCarrer } from "./app.interface";
+export { Imodel } from "./model.interface";
+export { Irepository } from "./repository.interface";
+export { Icontroller } from "./controller.interface";
