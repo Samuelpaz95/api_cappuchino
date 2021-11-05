622151d79f91f8351a9adaa4b2b525953c6f935a
Alan Benavides <bruno@coastt.org>
2021-11-05 15:00:27 -0400

    add: implement professors repository

--- src/modules/professors/interfaces/repository.interface.ts
+++ src/modules/professors/interfaces/repository.interface.ts
@@ -1,3 +1,9 @@
-interface Irepository {}
+import { ImapProfessors, Iprofessors } from ".";
+
+interface Irepository {
+	getAllProfessors(department: string): Promise<Iprofessors | null>;
+
+	getProfessorSubjects(department: string): Promise<ImapProfessors | null>;
+}
 
 export { Irepository };
--- src/modules/professors/repository.ts
+++ src/modules/professors/repository.ts
@@ -1,5 +1,37 @@
-import { Irepository } from "./interfaces";
+import { Isubject } from "../shared/interfaces";
+import { ImapProfessors, Iprofessors, Irepository } from "./interfaces";
+import { readFile } from "fs";
+import { resolve as pathResolve } from "path";
 
-class Repository implements Irepository {}
+class Repository implements Irepository {
+	private readonly route: string = "./public/data/professors";
+
+	getAllProfessors(department: string): Promise<Iprofessors | null> {
+		return new Promise<Iprofessors | null>((resolve, reject) => {
+			readFile(pathResolve(`${this.route}/${department}/teachers.json`), (err, data) => {
+				if (err) {
+					console.log(err);
+					reject(null);
+				}
+				resolve(JSON.parse(data.toString()));
+			});
+		});
+	}
+
+	getProfessorSubjects(department: string): Promise<ImapProfessors | null> {
+		return new Promise<ImapProfessors | null>((resolve, reject) => {
+			readFile(
+				pathResolve(`${this.route}/${department}/teachersContent.json`),
+				(err, data) => {
+					if (err) {
+						console.log(err);
+						reject(null);
+					}
+					resolve(JSON.parse(data.toString()));
+				}
+			);
+		});
+	}
+}
 
 export default Repository;
