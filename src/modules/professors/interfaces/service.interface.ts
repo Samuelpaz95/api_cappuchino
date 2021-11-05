426612bff1cf20d97354a8bac10ba37ee22ba7a7
Alan Benavides <bruno@coastt.org>
2021-11-05 15:08:27 -0400

    add: implement professor service

--- src/modules/professors/interfaces/service.interface.ts
+++ src/modules/professors/interfaces/service.interface.ts
@@ -1,3 +1,10 @@
-interface Iservice {}
+import { Iprofessors } from ".";
+import { Isubject } from "../../shared/interfaces";
+
+interface Iservice {
+	getAllProfessors(department: string): Promise<Iprofessors | null>;
+
+	getProfessorSubjects(department: string, professor: string): Promise<Isubject | null>;
+}
 
 export { Iservice };
--- src/modules/professors/service.ts
+++ src/modules/professors/service.ts
@@ -1,10 +1,24 @@
-import { Irepository, Iservice } from "./interfaces";
+import { Isubject } from "../shared/interfaces";
+import { Iprofessors, Irepository, Iservice } from "./interfaces";
 
 class Service implements Iservice {
 	private repository: Irepository;
 
 	constructor(repository: Irepository) {
 		this.repository = repository;
+	}
+
+	getAllProfessors(department: string): Promise<Iprofessors | null> {
+		return this.repository.getAllProfessors(department);
+	}
+
+	async getProfessorSubjects(
+		department: string,
+		professor: string
+	): Promise<Isubject | null> {
+		const professorSubjects = await this.repository.getProfessorSubjects(department);
+		if (professorSubjects == null) return null;
+		return professorSubjects[professor] || null;
 	}
 }
 
