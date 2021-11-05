da167ebfc7ca179a209434750ea7ddb526db75aa
Alan Benavides <bruno@coastt.org>
2021-11-05 15:00:10 -0400

    add: export app professors interfaces

--- src/modules/professors/interfaces/index.ts
+++ src/modules/professors/interfaces/index.ts
@@ -1,3 +1,4 @@
 export { Icontroller } from "./controller.interface";
 export { Iservice } from "./service.interface";
 export { Irepository } from "./repository.interface";
+export { ImapProfessors, Iprofessors } from "./app.interface";
