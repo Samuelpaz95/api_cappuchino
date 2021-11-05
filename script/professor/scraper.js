011d0530cc9f0ac614941a9d6203d7e2fa1f5933
Alan Benavides <bruno@coastt.org>
2021-11-05 11:43:13 -0400

    add: get subjects by professors

--- script/professor/scraper.js
+++ script/professor/scraper.js
@@ -3,7 +3,6 @@ function professorsByDeparment(subjectsByDepartments) {
 
 	Object.entries(subjectsByDepartments).forEach(([deparment, subjects]) => {
 		const professors = new Set();
-		console.log(subjects);
 
 		subjects.forEach(({ groups }) => {
 			groups.forEach(({ teacher }) => {
@@ -17,6 +16,38 @@ function professorsByDeparment(subjectsByDepartments) {
 	return professorsByDeparment;
 }
 
+function subjectsByProfessors(subjects, professors) {
+	const objProfessors = objectProfessors(professors);
+
+	subjects.forEach((subject) => {
+		subject.groups.forEach((group) => {
+			const ownGroups = subject.groups.filter(
+				({ teacher }) =>
+					teacher == group.teacher &&
+					!objProfessors[group.teacher].find(({ name }) => subject.name == name)
+			);
+			// hay grupos que se repiten :c
+			if (ownGroups.length > 0) {
+				objProfessors[group.teacher].push({
+					...subject,
+					groups: ownGroups,
+				});
+			}
+		});
+	});
+
+	return objProfessors;
+}
+
+function objectProfessors(professors) {
+	const obj = {};
+	professors.forEach((professor) => {
+		obj[professor] = [];
+	});
+	return obj;
+}
+
 module.exports = {
 	professorsByDeparment,
+	subjectsByProfessors,
 };
