67a98fcf83f153c3332243263c38635fdb6cbe6b
Alan Benavides <bruno@coastt.org>
2021-11-05 15:21:44 -0400

    add: mount routes and module professors

--- src/modules/professors/routes.ts
+++ src/modules/professors/routes.ts
@@ -3,14 +3,21 @@ import { Express } from "express";
 
 class Routes {
 	private routeController: Icontroller;
-	private breakPoint = "/departments";
+	private breakPoint = "/professors";
 
 	constructor(app: Express, routeController: Icontroller) {
 		this.routeController = routeController;
 		this.configureRoutes(app);
 	}
 
-	private configureRoutes(app: Express) {}
+	private configureRoutes(app: Express) {
+		app
+			.route(`${this.breakPoint}/:department`)
+			.get(this.routeController.getAllProfessors);
+		app
+			.route(`${this.breakPoint}/:department/:professor`)
+			.get(this.routeController.getProfessorSubjects);
+	}
 }
 
 export default Routes;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import { Express } from "express";
 
 import { Module as departments } from "../modules/departments";
+import { Module as professors } from "../modules/professors";
 
 export default function (app: Express) {
 	const dM = new departments(app);
+	const pM = new professors(app);
 }
